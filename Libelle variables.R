c61849dcdc9db575e81cb9a48835a56daf3840eb
Idrissa dabo <ididabo@gmail.com>
2021-10-03 01:52:07 +0000

    calcul de SCA et LhCS pour l'année 2017 PDM1

---
+++ Libelle variables.R
@@ -0,0 +1,42 @@
+
+# Céréales et tubercules ---------------------------------------------------
+# FCStap céréales et tubercules
+
+
+# Légumineuses et noix ----------------------------------------------------
+# FCSPulse Légumineuses/noix
+
+
+# Lait et produits laitiers -----------------------------------------------
+# FCSDairy Lait et produits laitiers
+
+
+
+# Viande, poisson et oeufs ------------------------------------------------
+# FCSPr viande oeuf poisson (chèvre, boeuf, poulet, porc, poisson y compris Thon)
+# FCSPrMeatF Viande rouge/chair(Boeuf, porc agneau, chèvre, lapin)
+# FCSPrMeatO Viande d'organe (foie, reins, coeur et autres abats)
+# FCSPrFish Poissons et coquillage (poissons y compris thon, escargot)
+# FCSPrEgg Oeufs
+
+# Légumes -----------------------------------------------------------------
+
+
+# FCSveg Légumes et Feuilles (épinards, oignons, tomates, carottes, poivrons, haricotes verts)
+# FSCvegOrg Légumes oranges (légumes riches en vitamines carottes, poivron rouge, patate douce orange ect...)
+# FCSVegGre Légumes à feuilles vertes(épinards, brocoli, amarante et/ou autres feuilles vert foncé , feuilles de manioc)
+# FCSVegOth Autres Légumes (Oignon, tomate, concombre, radis, haricots verts, pois, etc. )
+
+# Fruits ------------------------------------------------------------------
+# FCSFruit Fruits (banane, pomme, citron, mangue, papaye, abricot, pêche, etc)
+# FCSFruitOrg Fruits Orange (fruits riches en vitamine A : mange, papaye, abricot, pêche)
+# FCSFruitOth Autres Fruits (banane, pomme, citron, mandarine)
+
+# Huiles et graisses ------------------------------------------------------
+# FCSFat Huile/matières grasses (huile végétale, huile de palme, beurre de karité, margarine, autres huiles / matières grasses)
+
+# Sucres ------------------------------------------------------------------
+# FCSSugar Sucres ou sucreries (sucre, miel, confiture, gâteau, bonbons, biscuits, viennoiserie et autres produits sucrés (boissons sucrées))
+
+# Condiments --------------------------------------------------------------
+# FCSCond Condiments
---
+++ script LessonsLearnt2021.R
@@ -0,0 +1,89 @@
+
+# Import de package -------------------------------------------------------
+
+library(tidyverse)
+library(fs)
+library(haven)
+library(here)
+library(labelled)
+library(writexl)
+
+# import des Différents ---------------------------------------------------
+setwd("C:/Users/DELL/Pictures/Dossier R/lessonslearnt2021/Data")
+List_bases <- dir_ls(regexp = ".sav") %>% 
+  map(read_sav)
+nom_base <- names(List_bases)
+nom_base <- str_replace(string = nom_base,pattern = ".sav", replacement = "")
+names(List_bases) <- nom_base
+list2env(List_bases, .GlobalEnv)
+
+
+
+
+# Base 2017 ---------------------------------------------------------------
+
+
+# Partie SCA --------------------------------------------------------------
+
+
+# Codebook base ménage
+Base_menagePDM1_Conjoint_2017<- to_factor(Base_menagePDM1_Conjoint_2017)
+codebook_PDM12017 <- var_label(Base_menagePDM1_Conjoint_2017)
+codebook_PDM12017 <- as.data.frame(do.call(rbind,codebook_PDM12017))
+codebook_PDM12017 <- codebook_PDM12017 %>% rownames_to_column()
+
+# write_xlsx(codebook_PDM12017, "codebook_PDM12017.xlsx")
+# remplacer les données manquantes par zéro
+
+Base_menagePDM1_Conjoint_2017 <- Base_menagePDM1_Conjoint_2017 %>% 
+  mutate(
+    caa2 = replace_na(caa2, 0),
+    FCSPulse = replace_na(cac2, 0),
+    cab2 = replace_na(cab2, 0),
+    FCSVegOrg = replace_na(cad2, 0),
+    FCSVegGre = replace_na(cae2, 0),
+    FCSVegOth = replace_na(caf2, 0),
+    FCSFruitOrg = replace_na(cag2, 0),
+    FCSFruitOth = replace_na(cah2, 0),
+    FCSPrMeatF = replace_na(cai2, 0),
+    FCSPrMeatO = replace_na(caj2, 0),
+    FCSPrFish = replace_na(cak2, 0),
+    FCSPrEgg = replace_na(cal2, 0),
+    FCSDairy = replace_na(cam2, 0),
+    FCSSugar = replace_na(cao2, 0),
+    FCSFat = replace_na(can2, 0),
+    FCSCond = replace_na(cap2, 0)
+  )  
+
+Base_menagePDM1_Conjoint_2017 <- Base_menagePDM1_Conjoint_2017 %>% 
+  mutate(
+    FCStap = caa2 + cab2, # Céréale et tubercules
+    FCSPulse = FCSPulse, #Légumineuse/noix
+    FCSVeg = FCSVegOrg + FCSVegGre + FCSVegOth, # Sommer pour avoir légumes,
+    FCSPr = FCSPrMeatF + FCSPrMeatO + FCSPrFish + FCSPrEgg,  # Sommer pour avoir protéines
+    FCSDairy = FCSDairy,
+    FCSSugar = FCSSugar,
+    FCSCond = FCSCond,
+    FCSFruit = FCSFruitOrg + FCSFruitOth
+  ) %>% mutate(
+    FCSFruit = ifelse(FCSFruit > 7 , 7 , FCSFruit),
+    FCSVeg = ifelse(FCSVeg > 7 , 7, FCSVeg),
+    FCSPr = ifelse(FCSPr > 7 , 7, FCSPr),
+    FCStap = ifelse(FCStap > 7 , 7, FCStap)
+  )
+
+Base_menagePDM1_Conjoint_2017 <- Base_menagePDM1_Conjoint_2017 %>% 
+  mutate( FCS = (2*FCStap) + (3*FCSPulse) + FCSVeg + FCSFruit + (4*FCSPr) + (4*FCSDairy) + (0.5*FCSFat) + (0.5*FCSSugar))
+
+Base_menagePDM1_Conjoint_2017 <- Base_menagePDM1_Conjoint_2017 %>% 
+  mutate(FCSCat28 = case_when(
+    FCS <= 28 ~ "Poor", 
+    between(FCS, 28.5, 42) ~ "Borderline", 
+    FCS > 42 ~ "Acceptable"
+  ))
+
+funModeling::freq(Base_menagePDM1_Conjoint_2017, "FCSCat28")
+
+# Partie Stratégies d'adaptation aux moyens d'existence -------------------
+
+
